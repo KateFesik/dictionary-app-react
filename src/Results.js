3a54d6d83ed214467a90a525429bdf938d3eabc6
KateFesik <109290179+KateFesik@users.noreply.github.com>
2022-10-12 11:32:01 +0200

    create Meaning.js

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,19 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
   console.log(props.results);
 
   return (
     <div className="Results">
-      {props.results.meanings[0].definitions[0].definition}
+      <h2>{props.results.word}</h2>
+      {props.results.meanings.map(function (meaning, index) {
+        return (
+          <div key={index}>
+            <Meaning meaning={meaning} />
+          </div>
+        );
+      })}
     </div>
   );
 }
